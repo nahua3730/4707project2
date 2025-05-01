0a26f15f5de986f420b98379002c85624215287b
nahua3730 <nahua3730@gmail.com>
2025-05-01 14:51:12 -0500

    Update Inventory.sql

--- Inventory.sql
+++ Inventory.sql
@@ -2,7 +2,7 @@ CREATE TABLE Inventory (
     B_ID INT PRIMARY KEY,
     quantity INT,
     threshold INT,
-    EID INT,
+    E_ID INT,
     FOREIGN KEY (B_ID) REFERENCES Books(B_ID),
-    FOREIGN KEY (EID) REFERENCES Employees(E_ID)
+    FOREIGN KEY (E_ID) REFERENCES Employees(E_ID)
 );
