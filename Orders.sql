c0e56f88e41782354d301b243da92869e7787dda
YejinShin-1201 <shin0390@umn.edu>
2025-05-02 17:19:54 -0500

    Update Orders.sql

--- Orders.sql
+++ Orders.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Orders (
-    O_ID INT PRIMARY KEY,
-    C_ID INT,
+    O_ID INT NOT NULL,
+    C_ID INT NOT NULL,
     ODate DATE,
     SDate DATE,
     Pstatus VARCHAR(20),
-    Price DECIMAL(10, 2),
-    Quantity INT,
+    Price DECIMAL(10, 2) NOT NULL,
+    Quantity INT NOT NULL,
+    PRIMARY KEY (O_ID),
     FOREIGN KEY (C_ID) REFERENCES Customers(C_ID)
 );
