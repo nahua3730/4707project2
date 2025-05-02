3534da276ca48fbae793f3f9859d4fff7a16672b
YejinShin-1201 <shin0390@umn.edu>
2025-05-02 17:18:33 -0500

    Update Inventory.sql

--- Inventory.sql
+++ Inventory.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Inventory (
-    B_ID INT PRIMARY KEY,
-    quantity INT,
+    B_ID INT NOT NULL,
+    quantity INT NOT NULL,
     threshold INT,
     E_ID INT,
+    PRIMARY KEY (B_ID),
     FOREIGN KEY (B_ID) REFERENCES Books(B_ID),
     FOREIGN KEY (E_ID) REFERENCES Employees(E_ID)
 );
