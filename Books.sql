8a5cf85bed0b7c43cfb5d2ded843616610e78e39
YejinShin-1201 <shin0390@umn.edu>
2025-05-02 17:13:26 -0500

    Update Books.sql

--- Books.sql
+++ Books.sql
@@ -1,11 +1,12 @@
 CREATE TABLE Books (
-    B_ID INT PRIMARY KEY,
-    title VARCHAR(100),
+    B_ID INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
     PublicationYear INT,
     genre VARCHAR(50),
-    price DECIMAL(10, 2),
+    price DECIMAL(10, 2) NOT NULL,
     PubName VARCHAR(15),
     PubContact VARCHAR(100),
     stock INT,
     Ithreshold INT
+    PRIMARY KEY (B_ID)
 );
