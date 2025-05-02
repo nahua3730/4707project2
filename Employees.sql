3e30342cabfe56a622e1b09f1f314af429ba14d9
YejinShin-1201 <shin0390@umn.edu>
2025-05-02 16:48:45 -0500

    Update Employees.sql

--- Employees.sql
+++ Employees.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Employees (
     E_ID INT PRIMARY KEY,
-    FName VARCHAR(50),
-    LName VARCHAR(50),
+    FName VARCHAR(15),
+    LName VARCHAR(15),
     hiredate DATE,
     position VARCHAR(50),
     super_EID INT,
