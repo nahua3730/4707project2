5fcfc2bdba73dcaea7bd721f80b3cf214d29be41
nahua3730 <nahua3730@gmail.com>
2025-05-02 18:26:52 -0500

    Update Employees.sql

--- Employees.sql
+++ Employees.sql
@@ -2,7 +2,7 @@ CREATE TABLE Employees (
     E_ID INT NOT NULL UNIQUE,
     FName VARCHAR(15) NOT NULL,
     LName VARCHAR(15) NOT NULL,
-    hiredate DATE,
+    hiredate DATE NOT NULL,
     position VARCHAR(50) NOT NULL,
     super_EID INT,
     PRIMARY KEY (E_ID),
