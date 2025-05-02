a34af5a1553d7d4342abdbc026dfed4e79b7e645
YejinShin-1201 <shin0390@umn.edu>
2025-05-02 16:48:30 -0500

    Update Customers.sql

--- Customers.sql
+++ Customers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Customers (
     C_ID INT PRIMARY KEY,
-    FName VARCHAR(50),
-    LName VARCHAR(50),
+    FName VARCHAR(15),
+    LName VARCHAR(15),
     phone VARCHAR(20),
     emailAddr VARCHAR(100),
     billingAddr TEXT,
