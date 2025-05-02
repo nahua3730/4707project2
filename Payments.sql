ced127a1b7464033fbfd5d69e0cbedf655abc2ff
YejinShin-1201 <shin0390@umn.edu>
2025-05-02 17:20:57 -0500

    Update Payments.sql

--- Payments.sql
+++ Payments.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Payments (
-    P_ID INT PRIMARY KEY,
-    C_ID INT,
-    amount DECIMAL(10, 2),
+    P_ID INT NOT NULL,
+    C_ID INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
     method VARCHAR(50),
     paymentdate DATE,
-    O_ID INT,
+    O_ID INT NOT NULL,
+    PRIMARY KEY (P_ID),
     FOREIGN KEY (C_ID) REFERENCES Customers(C_ID),
     FOREIGN KEY (O_ID) REFERENCES Orders(O_ID)
 );
